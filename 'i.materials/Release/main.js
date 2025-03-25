4aa2454421bad2e3a2207d4c591a0b102f501539
Yrahcaz7 <zachary.shive@gmail.com>
2025-03-24 21:11:27 -0400

    website bugfixes

--- 'i.materials/Release/main.js
+++ 'i.materials/Release/main.js
@@ -1,19 +1,11 @@
 let darkened = false;
 
-function setProperty(property, value) {
-	document.documentElement.style.setProperty(property, value);
-};
-
-const THEME_PROPERTIES = ["body-fg", "body-bg", "link-normal", "link-visited", "link-hover", "user-1"];
+const THEME_PROPERTIES = ["terp-fg", "terp-bg", "terp-input", "header-color", "body-fg", "body-bg", "link-normal", "link-visited", "link-hover", "user-1"];
 
 function toggleDarkness() {
-	const key = "var(--" + (darkened ? "light" : "dark");
-	setProperty("--terp-fg", key + "-fg)");
-	setProperty("--terp-bg", key + "-bg)");
-	setProperty("--terp-input", key + "-input)");
-	setProperty("--header-color", key + "-header)");
+	const theme = (darkened ? "light" : "dark");
 	THEME_PROPERTIES.forEach(property => {
-		setProperty("--" + property, key + "-" + property + ")");
+		document.documentElement.style.setProperty("--" + property, "var(--" + theme + "-" + property + ")");
 	});
 	darkened = !darkened;
 };
